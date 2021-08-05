f4f9a513196c1ef88abd8c5ea2a490606122c2e3
kamichristella <kamichristella@gmail.com>
2021-08-05 11:40:37 +0200

    Heroku application

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-
-
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//        console.log(data)
-//     })
-// })
-
-
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
@@ -20,16 +11,14 @@ messageTwo.textContent=''
     if(!location){
        messageOne.textContent='Please enter the location'
     }else{
-        fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+        fetch(`/weather?address=${location}`).then((response)=>{
             response.json().then((data)=>{
                 if(data.error){
-                    // console.log(data.error)
                     messageOne.textContent=data.error
                 }else{
                     messageOne.textContent=data.location
                     messageTwo.textContent=data.forecast
-                    // console.log(data.location)
-                    // console.log(data.forecast)
+                   
                 }
             })
         })
